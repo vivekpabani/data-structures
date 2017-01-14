a2f117de4465dce899d6d9618c6c1ebfa2bf0496
Vivek Pabani <vpabani@hawk.iit.edu>
2017-01-13 21:10:18 -0600

    implemented basic methods to find vertices and edges of graph.

--- python/graph/graph.py
+++ python/graph/graph.py
@@ -15,12 +15,62 @@ class Graph(object):
     def __init__(self, graph_dict = None)
         """
         Initialize the graph object with graph_dict.
-        :param graph_dict: dictionary in form of (node, list_of_nodes)
-                       where list_of_nodes have all nodes
-                       adjacent to the key node. 
+        :param graph_dict (str, list): dictionary in form of (node, list_of_nodes)
+               where list_of_nodes have all nodes adjacent to the key node.
         """
 
         if graph_dict is None:
             graph_dict = dict()
 
         self.__graph_dict = graph_dict 
+
+    def vertices(self):
+        """
+        Find the vertices of the graph.
+        :return (list): the vertices of the graph
+        """
+
+        return self.__graph_dict.keys()
+
+    def edges(self):
+        """
+        Find the edges of the graph by calling the generate_edges functions.  
+        :return (list): list of sets, where each set is a pairs of node of an edge.
+        """
+
+        return self.__generate_edges()
+
+    def __generate_edges(self):
+        """
+        Generate the edges of the graph by going through the graph_dict.
+        Edges are represented as sets with one (a loop back to the vertex) 
+        or two with one (a loop back to the vertex) or two vertices. 
+        :return (list): list of sets, where each set is an edge - two nodes of an edge. 
+
+        edges = list() 
+
+        for vertex in self.__graph_dict.keys():
+            for neighbour in self.__graph_dict[vertex]:
+                # if the reverse edge does not exist already
+                # then add the current one. 
+                if {neighbour, vertex} not in edges:
+                    edges.append({vertex, neighbour})
+
+        return edges
+
+    def __str__(self):
+        """
+        String representation of the graph.
+        to be used by print method.
+        :return (string): graph data as string.
+        """
+
+        result = "vertices:\n"
+        for vertex in self.__graph_dict.keys():
+            result = result + str(vertex) + " "
+
+        result = result + "\n\nEdges:\n"
+        for edge in self.__generate_edges():
+            result = result + str(edge) + " "
+
+        return result
